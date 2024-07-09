31d1e0e26aeebff200891216ce1b109e7b487c29
Dima Shirikov <dmitrsoko@gmail.com>
2024-07-09 20:49:14 +0300

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -23,12 +23,27 @@ ll inv(ll x) {
    return binary_pow(x, MOD - 2);
 } // ОБРАТНЫЙ ЭЛЕМЕНТ ПО МОДУЛЮ(ЕСЛИ НАШЕ ЧИСЛО ВЗАИМНО ПРОСТО С МОДУЛЕМ)
 
-ll c(int n, int k) {
+map<pair<ll, ll>, ll> cmp;
+
+ll c(ll n, ll k) {
    if (n == k || k == 0) {
+      cmp[{n, k}] = 1;
       return 1;
    }
-   return (c(n - 1, k - 1) + c(n - 1, k)) % MOD;
-} // РЕКУРЕНТНАЯ ФОРМУЛА С(N, K)
+   ll x = cmp[{n - 1, k - 1}];
+   ll y = cmp[{n - 1, k}];
+   if (x == 0) {
+      x = c(n - 1, k - 1);
+      cmp[{n - 1, k - 1}] = x;
+   }
+   if (y == 0) {
+      y = c(n - 1, k);
+      cmp[{n - 1, k}] = y;
+   }
+   cmp[{n, k}] = x + y;
+   return (x + y);
+   
+} // РЕКУРЕНТНАЯ ФОРМУЛА С(N, K) с мемоизацией
 
 int d(int n) {
    if (n == 1) {
